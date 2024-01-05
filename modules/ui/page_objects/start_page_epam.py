01ea98c91036ec99ae92746b37e5985c2863e3e4
Stest <stestingqa@gmail.com>
2024-01-05 21:52:00 +0200

    Finish Check that allow to change language to UA

--- modules/ui/page_objects/start_page_epam.py
+++ modules/ui/page_objects/start_page_epam.py
@@ -12,8 +12,11 @@ class StartEpamPage(BasePage):
     def go_to_epam(self):
         self.driver.get(StartEpamPage.URL)
     
-    def check_epam_title(self, exp_title):
-        return self.driver.title == exp_title
+    def check_epam_title(self, exp_title, timeout = 3):
+        WebDriverWait(self.driver, timeout).until(
+            EC.title_is(exp_title)
+        )
+        assert self.driver.title == exp_title
 
     def get_theme(self):
         header_element = self.driver.find_element(By.CLASS_NAME, 'fonts-loaded')
@@ -23,20 +26,22 @@ class StartEpamPage(BasePage):
     def toogle_switch_elem(self):
         switch = self.driver.find_element(By.CLASS_NAME, "switcher")
         
-    def get_location(self):
+    def get_lang_location(self):
         global_language_button = self.driver.find_element(By.CLASS_NAME, 'location-selector__button')
         global_language_button.click()
-
-    def check_location_title(self, location_language_title):
-        return self.driver.title == location_language_title
         
-        #find UA element on dropdown menu
     def change_language(self):
         ua_link_location = (By.XPATH, '//a[@class="location-selector__link" and @href="https://careers.epam.ua"]')
         ua_language = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable(ua_link_location)
         )
         ua_language.click()
+    
+    def check_header_title(self, header_expected_text):
+        vacancies_link = self.driver.find_element(By.CLASS_NAME, "top-navigation__item-link")
+        vacancies_text = vacancies_link.text
+        assert vacancies_text == header_expected_text
+
     
 
 
--- tests/ui/test_epam_ui.py
+++ tests/ui/test_epam_ui.py
@@ -1,8 +1,7 @@
 from modules.ui.page_objects.start_page_epam import StartEpamPage
 import pytest
-import time
 
-@pytest.mark.sa
+@pytest.mark.epamui
 def test_check_the_title_is_correct():
     # Create object page
     start_page_epam = StartEpamPage()
@@ -41,7 +40,7 @@ def test_the_ability_to_switch_theme():
     start_page_epam.quit_driver()
 
 
-@pytest.mark.sa
+@pytest.mark.epamui
 def test_that_allow_to_change_lang_to_ua():
     # Create object page
     start_page_epam = StartEpamPage()
@@ -50,14 +49,17 @@ def test_that_allow_to_change_lang_to_ua():
     start_page_epam.go_to_epam()
 
     #Find location and open
-    start_page_epam.get_location()
-
-    start_page_epam.check_location_title("Select a language")
+    start_page_epam.get_lang_location()
 
     start_page_epam.change_language()
 
-    time.sleep(5)
-
     start_page_epam.check_epam_title("EPAM Ukraine - найбільша ІТ-компанія в Україні | Вакансії")
-
+    
+    start_page_epam.check_header_title("ВАКАНСІЇ")
+    start_page_epam.check_header_title("НАВЧАННЯ")
+    start_page_epam.check_header_title("БЛОГ")
+    start_page_epam.check_header_title("ВАКАНСІЇ")
+    start_page_epam.check_header_title("ПОДІЇ")
+    start_page_epam.check_header_title("ПРО КОМПАНІЮ")
+    
     start_page_epam.quit_driver()
